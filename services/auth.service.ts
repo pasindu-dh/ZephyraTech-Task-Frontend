40d1d3faf04f0a900502638162baccc1b6e41462
Pasindu Dilshan Hettiarachchi <pasindud265@gmail.com>
2026-06-10 17:22:35 +0530

    Update API_URL in AuthService to use production backend

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://zephyratech-task-backend.onrender.com/api/auth/";
 
 class AuthService {
   login(email: string, password: string): Promise<any> {
